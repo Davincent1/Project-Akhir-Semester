2718cc52473b9c5b474a32312ccc57035ad248e2
Davincent1 <davincentlee008@gmail.com>
2024-11-23 10:05:36 +0700

    Update Project.py

--- Project Akhir Semester/Project.py
+++ Project Akhir Semester/Project.py
@@ -27,10 +27,12 @@ def main():
     senjataKatana = senjata("Katana")
     senjataBusur = senjata("Busur")
     senjataPalu = senjata("Palu Besi")
+    senjataTongkat = senjata("Tongkat Bola Api")
 
     armorTipe = armor("Titanium")
     armorTipe2 = armor("Besi")
     armorTipe3 = armor("Emas")
+    armorTipe4 = armor("Perak")
 
     newPlayer1 = player( "Steve", senjataBusur.useSenjata(), armorTipe.usearmor() )
     newPlayer1.display()
@@ -40,6 +42,9 @@ def main():
     
     newPlayer3 = player("Martin", senjataKatana.useSenjata(), armorTipe3.usearmor() )
     newPlayer3.display()
+    
+    newPlayer4 = player("Timothy", senjataTongkat.useSenjata(), armorTipe4.usearmor() )
+    newPlayer4.display()
 
     print(f"Jumlah Object :  { player.tempCount } ")
 
